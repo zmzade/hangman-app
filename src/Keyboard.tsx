21f2353a819cdbc5333d2bcab91b12ea175835b6
zmzade <zmahmoodzadeh@yahoo.com>
2022-11-07 12:12:35 +0100

    adding hagman files

---
+++ src/Keyboard.tsx
@@ -0,0 +1,72 @@
+import styles from "./Keyboard.module.css";
+const KEYS = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+type KeyboardProps = {
+  disabled?: boolean;
+  correctLetters: string[];
+  incorrectLetters: string[];
+  addGuessedLetters: (letter: string) => void;
+};
+
+const Keyboard = ({
+  disabled = false,
+  correctLetters,
+  incorrectLetters,
+  addGuessedLetters,
+}: KeyboardProps) => {
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
+        gap: ".5rem",
+      }}
+    >
+      {KEYS.map((key) => {
+        const isActive = correctLetters.includes(key);
+        const isInactive = incorrectLetters.includes(key);
+        return (
+          <button
+            onClick={() => addGuessedLetters(key)}
+            className={`${styles.btn} ${isActive ? styles.active : ""} ${
+              isInactive ? styles.inactive : ""
+            }`}
+            disabled={isActive || isInactive || disabled}
+            key={key}
+          >
+            {key}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Keyboard;
